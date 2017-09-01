8a1370fbdc43178a1c51e8e092fc5d1cbe7aa134
Ana María Martínez Gómez <ammartinez@suse.de>
2017-09-01 10:21:05 +0200

    Last day sprint 23

---
+++ burndow-charts/current/script.sh
@@ -0,0 +1,5 @@
+trollolo burndown
+trollolo plot 23
+git commit -m 
+curl --request POST --url "https://api.trello.com/1/cards/W7MggXCf/actions/comments?text=hola2&key=38f60c92ee8e517dae7d7369480f981f&token=b8506de22f37b24d26a22705ee8d1c2b551ef6cb40c912f310b5538859af906e"
+curl --request POST --url "https://api.trello.com/1/cards/W7MggXCf/actions/attachments?&file=burndown-23.png&key=38f60c92ee8e517dae7d7369480f981f&token=b8506de22f37b24d26a22705ee8d1c2b551ef6cb40c912f310b5538859af906e"
